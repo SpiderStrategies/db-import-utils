1dce586055df696db7d9aa326b25417939a1178d
jason.mckim <jason.mckim@spiderstrategies.com>
2019-01-14 10:03:09 -0500

    need encryption utills

--- action-test/entrypoint.sh
+++ action-test/entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh -l
-id
-su root
-id
-echo $PATH
-pwd
-ls /home
-ls -la
-gradle --debug test
-sudo gradle --debug test
-chmod -R 777 *
-gradle --debug test
+
+cd /github/
+git clone git@github.com:SpiderStrategies/encryption-utils.git
+cd /github/encryption-utils
+gradle fatJar
+cd /github/workspace
+gradle test
