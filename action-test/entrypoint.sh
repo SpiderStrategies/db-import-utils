70cbf4be6816867067b22bbb371791d4139c427a
jason.mckim <jason.mckim@spiderstrategies.com>
2019-01-14 10:26:20 -0500

    use proper case for curl args

--- action-test/entrypoint.sh
+++ action-test/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 cd /github/
-curl https://github.com/SpiderStrategies/encryption-utils/archive/master.zip -O /github/
+curl https://github.com/SpiderStrategies/encryption-utils/archive/master.zip -o /github/master.zip
 unzip master.zip
 mv master encryption-utils
 cd /github/encryption-utils
